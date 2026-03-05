909c765e466336535c875d68170634f0de9cbca8
kaiser-114 <areezilla@gmail.com>
2026-03-05 15:24:30 +0700

    implement input function for bubble sort

---
+++ bubble_sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20];
+int n;
+
+void input() 
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array: ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\nArray dapat memiliki maksimal 20 elemen.\n";
+    }
+
+    cout << "\n===================" << endl;
+    cout << "Masukkan Elemen Array" << endl;
+    cout << "===================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
