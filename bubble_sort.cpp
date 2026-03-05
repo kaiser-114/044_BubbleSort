029f91732d0c20cf17969fb5a74e536a311e1b0d
kaiser-114 <areezilla@gmail.com>
2026-03-05 15:57:13 +0700

    add implement bubblesortArray

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -18,13 +18,41 @@ void input()
             cout << "\nArray dapat memiliki maksimal 20 elemen.\n";
     }
 
-    cout << "\n===================" << endl;
+    cout << "=====================" << endl;
     cout << "Masukkan Elemen Array" << endl;
-    cout << "===================" << endl;
+    cout << "=====================" << endl;
 
-    for (int i = 0; i < n; i++)
+    for (int k = 0; k < n; k++)
     {
-        cout << "Data ke-" << (i + 1) << ": ";
-        cin >> arr[i];
+        cout << "Data ke-" << (k + 1) << ": ";
+        cin >> arr[k];
     }
 }
+
+void bubbleSortArray()
+{
+    int pass = 1;
+
+    do 
+    {
+        for (int k = 0; k < n - pass; k++)
+        {
+            if (arr[k] > arr[k + 1])
+            {
+                int temp = arr[k];
+                arr[k] = arr[k + 1];
+                arr[k + 1] = temp;
+            }
+        }
+        pass += 1;
+
+        cout << "Pass " << (pass - 1) << ": ";
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " ";
+        }
+        cout << endl;
+
+    } while (pass < n - 1);
+}
+
